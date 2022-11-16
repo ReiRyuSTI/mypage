68074ddb564a43e7bde7f222b4b1f59147a7a04e
ReiRyuSTI <ry-tanaka@sios.com>
2022-11-16 11:16:02 +0900

    vol3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
@@ -7,8 +9,24 @@ module.exports = {
         kosugi: ["Kosugi"],
         lobster: ["Lobster"],
         dela: ["Dela Gothic One"],
+        syuku: ["Yuji Syuku"],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, addComponents, e, prefix, config }) {
+      const newUtilities = {
+        ".horizontal-tb": {
+          writingMode: "horizontal-tb",
+        },
+        ".vertical-rl": {
+          writingMode: "vertical-rl",
+        },
+        ".vertical-lr": {
+          writingMode: "vertical-lr",
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
 };
